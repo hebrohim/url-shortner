d834fa7bafb571de253bf940820ddc98bbf99cd0
hebrohim <raheemibrohim@mail.com>
2023-04-08 20:03:17 +0000

    feat:add animation to nav and hero sectiont

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,42 @@
 import React from "react";
+import { motion } from "framer-motion";
 import backgroundImage from "../images/illustration-working.svg";
+
+
+//HERO SECTION ANIMATION
+const heroAnimation = {
+  hidden: { opacity: 0 ,x:"-10vw"},
+  visible: { opacity: 1 ,x:0, transition:{duration:1}},
+  
+};
+
+
 const Header = () => {
   return (
-    <section className=" h-[90vh] flex">
+    <motion.section variants={heroAnimation} initial="hidden" animate = "visible" className=" h-[90vh] mt-10">
+      <div className="flex flex-col-reverse items-center  ml-5 md:flex-row md:ml-20">
+        <div className="heroText mt-5 flex flex-col justify-center items-center md:items-start">
+          <h1 className=" font-textFont text-4xl  text-gray-800 font-bold text-center md:text-left md:text-5xl">
+            More than just shorter links
+          </h1>
+          <p className="text-center md:text-left md:text-xl">
+            Build your brand's recognition and get detailed insights on how your
+            links are performing
+          </p>
 
-<div className="flex flex-col-reverse items-center  ml-5 md:flex-row md:ml-20">
-<div className="heroText mt-5 flex flex-col justify-center items-center md:items-start">
-  <h1 className=" font-textFont text-4xl  text-gray-800 font-bold text-center md:text-left md:text-5xl">
-          More than just shorter links
-        </h1>
-        <p className="text-center md:text-left md:text-xl">Build your brand's recognition and get detailed insights on how your links are performing</p>
-
-        <button className="bg-[#19c9cf] box-border px-3 py-2 mt-5 rounded-3xl font-bold text-white">Get started</button>
+          <motion.button className="bg-[#19c9cf] box-border px-3 py-2 mt-5 rounded-3xl font-bold text-white">
+            Get started
+          </motion.button>
         </div>
         <div className=" heroImage overflow-hidden">
-        <img src={backgroundImage} className="-translate-x-[-25vw] md:-translate-x-[-10vw]" alt="" />
-     
-
-    
-       
-
+          <motion.img 
+            src={backgroundImage}
+            className="-translate-x-[-25vw] md:-translate-x-[-10vw]"
+            alt=""
+          />
+        </div>
       </div>
-      </div>
-    </section>
+    </motion.section>
   );
 };
 
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import logo from "../images/logo.svg"
 import {GiHamburgerMenu } from "react-icons/gi";
+import { motion } from 'framer-motion';
+
+//NAVBAR ANIMATION
+
+const navAnimation = {
+  hidden:{y:-200,opacity:0,skew:10},
+  visible:{y:0,opacity:1,skew:360,transition:{duration:1}}
+}
 const NavBar = () => {
   return (
-   <nav className='flex justify-between p-5 md:px-20'>
+   <motion.nav variants={navAnimation} initial= "hidden" animate="visible" className='flex justify-between p-5 md:px-20'>
   <img src={logo}/>
 <GiHamburgerMenu className='text-3xl text-gray-600'/>
 
 
-   </nav>
+   </motion.nav>
   )
 }
 
